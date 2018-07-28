87dcc8c85a51d9cce0258b3c0cfcca23bd9d76ec
Eli Cho <eli.cho@gmail.com>
2018-07-28 07:24:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,10 +2,9 @@ def roll_call_dwarves(array)
   array.each_with_index do |name|
     puts "#{array.index(name) + 1} #{name}"
   end
-  
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet
   # Your code here
 end
 
